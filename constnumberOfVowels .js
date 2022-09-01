6041ad929a982f62f84f5b98bca136b67723164a
tatianibassani <tatianibassani@gmail.com>
2022-09-01 16:55:43 +0000

    number of vowels in a string

---
+++ constnumberOfVowels .js
@@ -0,0 +1,14 @@
+const numberOfVowels = function(data) {
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i ++){
+    if(data[i] === "a" || data[i] === "e" || data[i] === "i" || data[i] === "o" || data[i] === "u"){
+      sum++;
+    }
+  }
+  return sum;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
